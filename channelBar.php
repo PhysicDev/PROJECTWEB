ae226a3aefa609bd68deb9cba9b78d7fd636c671
Azemard-Maxime <94792521+Azemard-Maxime@users.noreply.github.com>
2022-06-01 10:18:10 +0200

    Update channelBar.php

--- channelBar.php
+++ channelBar.php
@@ -7,17 +7,35 @@
 <div id="Lbar">
     <?php
         //si l'utilisateur est connecté on affiche le bouton pour créer un salon de discussion
+
+        if (isset($connected) && $connected) {
+            echo '<button class="button" onclick="showChannel()">Créer un salon de discussion</button>';
+        }
     ?>
     <ul>
         <?php
             //on lit le fichier csv pour afficher la liste des salons de discussion
             $nbSalons = 0;
-            echo("il y a $nbSalons salon de discution");
 
             //
              //   exemple d'element de liste : 
              //   <li><a href="channel.php?name= le nom du chanel"> nomDuChannel :: Admin : nomDuCreateur</a></li>
             //
+
+            $lesChannels = file("data/channel.csv");
+            $nbSalons = count($lesChannels);
+            echo("il y a $nbSalons salon de discution" . "<br>" . "<br>");
+            foreach ($lesChannels as $channel) {
+                $channel = explode("|", $channel);
+                $nameChannel = $channel[0];
+                $admin = $channel[1];
+                //$nbSalons++;
+                echo("<li><a href='channel.php?name=$nameChannel'>$nameChannel | Admin : $admin</a></li>");
+            }
+           
+           
+
+
         ?>
     </ul>
 </div>
