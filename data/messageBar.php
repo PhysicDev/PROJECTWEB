47badd275f1c51c60bc793f68b1c27a31ff9d46c
Azemard-Maxime <94792521+Azemard-Maxime@users.noreply.github.com>
2022-06-01 10:33:51 +0200

    message

---
+++ data/messageBar.php
@@ -0,0 +1,16 @@
+<!--
+    Un fichier qui affiche les messages des salons de discussions
+    positionner à droite de la page
+-->
+
+<div id = "Rbar" >
+    <?php
+    ?>
+
+    <ul>
+        
+    </ul>
+
+
+</div>
+
