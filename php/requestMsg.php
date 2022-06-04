dfb303b2557159246e316868be55dfee7845302a
PhysicDev <51998479+PhysicDev@users.noreply.github.com>
2022-06-04 14:58:46 +0200

    correctFinal

--- php/requestMsg.php
+++ php/requestMsg.php
@@ -6,11 +6,14 @@
         echo("<h2>canal : $channel </h2>");
         $lesMsg = file("../data/messages/$channel.txt");
         echo("<ul>");
+        $i=0;
         foreach ($lesMsg as $msg) {
             $msgEtPers = explode("|", $msg);
             $leMsg = $msgEtPers[1];
             $pers = $msgEtPers[0];
-            echo("<li>$pers : $leMsg</li>");
+            $i++;
+            //le truc avec le $i c'est pour le style css.
+            echo("<li class='".($i%2==0?"even":"odd")."'>$pers : $leMsg</li>");
         }     
         
         echo("</ul>");
@@ -42,4 +45,9 @@
     // Warning: file(data/messages/test1234.txt): Failed to open stream: No such file or directory in C:
     //   \XAMPP\htdocs\Application_du_Web\Projet\PROJECTWEB\php\requestMsg.php on line 7
 ?>
+<div id="textMessage">
+<textarea id="messageDat" placeholder="envoyer un message"></textarea>
+<img id="sendButton" src="images/send.png" type="submit" value="submit" onclick="sendForm()" />
+</div>
+</div>
 
